2cbd2f9bb852ea03937ad692f356c587e863869a
3dw1nM0535 <workockmoses@gmail.com>
2018-03-25 23:40:41 +0300

    tuple ds tutorials

---
+++ Tuples/tuples.py
@@ -0,0 +1,25 @@
+# Tuple is a sequence of immutable python objects
+# Unlike lists, tuples can not be changed and are
+# enclosed in parenthesis
+tuple1 = ("physics", "biology", 1995, True)
+tuple2 = "a", "b", "c", "d"
+tuple3 = (1,2,3,4,5)
+tuple4 = () # empty tuple
+tuple5 = (4,) # single value tuple should have a comma at the end
+
+# Accessing values in a tuple
+print "Accessing values in a tuple"
+print tuple1[3]
+print tuple2[0]
+print tuple3[1:3]
+
+# updating values in a tuple
+
+		# NOT OPERATIONAL
+
+# removing tuple
+# not operation on individual element but the entire tuple
+print "Deleting a whole tuple!!"
+del tuple4
+print "After deleting tuple4"
+print tuple4
